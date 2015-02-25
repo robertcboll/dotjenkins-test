0b912798394c37f6e6c63a9f3b2ec693e34133d6
Robert Boll <robertcboll@gmail.com>
2015-02-25 18:43:48 -0500

    need scm config

--- .jenkins.groovy
+++ .jenkins.groovy
@@ -1,6 +1,15 @@
 job {
   name "generated-test-build"
 
+  scm {
+    git {
+      remote {
+        url "https://github.com/robertcboll/dotjenkins-test"
+      }
+      branch "master"
+    }
+  }
+
   steps {
     shell "./build.sh"
   }
