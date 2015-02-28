c20ead97c6c5489f1eece694229008f84a179f24
Robert Boll <robertcboll@gmail.com>
2015-02-28 15:44:59 -0500

    no public here

--- .jenkins/config.groovy
+++ .jenkins/config.groovy
@@ -1,6 +1,6 @@
 import groovy.json.JsonSlurper
 
-public def json = new JsonSlurper().parseText(readFileFromWorkspace('.jenkins/jenkins.json'))
+def json = new JsonSlurper().parseText(readFileFromWorkspace('.jenkins/jenkins.json'))
 
 def cfg = {
   sample {
