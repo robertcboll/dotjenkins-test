1473feb3754f7b8d69a2a0acd1445e57c4c64d57
Robert Boll <robertcboll@gmail.com>
2015-02-28 20:20:14 -0500

    pipeline view

---
+++ .jenkins/pipeline-view.groovy
@@ -0,0 +1,14 @@
+import groovy.json.JsonSlurper
+def json = new JsonSlurper().parseText(readFileFromWorkspace('.jenkins/config.json'))
+
+def job_name      = 'pipeline'
+def job_desc      = 'pipeline'
+
+view(type: BuildPipelineView) {
+  name "${json.jenkins.folder}/${job_name}"
+  description job_desc
+
+  displayedBuilds 10
+
+  selectedJob "${json.jenkins.folder}/one"
+}
